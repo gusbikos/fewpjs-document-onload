692ec7da919ea8b88f8156553c60caa652c7ef04
gusbikos <gusbikos@gmail.com>
2021-04-03 22:21:56 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const pTag = document.querySelector('p#text')
+    pTag.textContent = "This is really cool!"
+})
+
